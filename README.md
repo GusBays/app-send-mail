e478d4cf9b0a9038b38b30391c921bad06bb3f8f
Gustavo Bays <gustavo_boliv@hotmail.com>
2022-02-18 00:37:47 -0300

    feat: finished front-end, add dynamic parameters to send the emails and made safety fixes on the project

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # app-send-mail
-Website created using PHP to send e-mails using smtpm gmail server.
-
+Website created using PHP to send e-mails using smtp gmail server.
 
 PHP Mailer required.
+DOCS: https://github.com/PHPMailer/PHPMailer/tree/v6.4.1
--- processa_envio.php
+++ processa_envio.php
@@ -12,6 +12,7 @@
         private $para = null;
         private $assunto = null;
         private $mensagem = null;
+        public $status = ['codigo_status' => null, 'descricao_status' => ''];
 
         public function __get($attr) {
             return $this->$attr;
@@ -39,15 +40,14 @@
     //print_r($mensagem);
 
     if(!$mensagem->mensagemValida()) {
-        echo 'Mensagem não é válida';
-        die();
+        header('Location: index.php');
     }
 
     $mail = new PHPMailer(true);
 
     try {
         //Server settings
-        $mail->SMTPDebug = 2;                                       //Enable verbose debug output
+        $mail->SMTPDebug = false;                                       //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
@@ -57,9 +57,9 @@
         $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         //Recipients
-        $mail->setFrom('phpsendmail7.4@gmail.com', 'PHP Send Mail App Sender');
-        $mail->addAddress('phpsendmail7.4@gmail.com', 'PHP Send Mail App Receiver');     //Add a recipient           //Name is optional
-        //$mail->addReplyTo('info@example.com', 'Information');         //Add another address if the receiver reply the message
+        $mail->setFrom('phpsendmail7.4@gmail.com', 'PHP Send Mail App');
+        $mail->addAddress($mensagem->__get('para'));                  //Add a recipient           //Name is optional
+        //$mail->addReplyTo('info@example.com', 'Information');       //Add another address if the receiver reply the message
         //$mail->addCC('cc@example.com');                             //Add another e-mail for copy
         //$mail->addBCC('bcc@example.com');                           //Add another e-mail as a hiden copy
 
@@ -69,15 +69,63 @@
 
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Assunto do email';
-        $mail->Body    = 'Este é um teste de corpo do <strong>email</strong>';
-        $mail->AltBody = 'Este é um teste de corpo do email sem tags HTML';
+        $mail->Subject = $mensagem->__get('assunto');
+        $mail->Body    = $mensagem->__get('mensagem');
+        $mail->AltBody = 'É necessário que você utilize um client que suporte HTML para visualizar todo o conteúdo desta mensagem.';
 
         $mail->send();
-        echo 'Mensagem enviada com sucesso';
+        $mensagem->status['codigo_status'] = 200;
+        $mensagem->status['descricao_status'] = 'E-mail enviado com sucesso';
+
     } catch (Exception $e) {
-        echo "Não foi possível enviar este email. Erro encontrado: {$mail->ErrorInfo}";
+        $mensagem->status['codigo_status'] = 500;
+        $mensagem->status['descricao_status'] = "Não foi possível enviar este email. Erro encontrado: {$mail->ErrorInfo}";
+
     }
+?>
 
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        
+        <title>App Send Mail</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="py-3 text-center">
+                <img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+                <h2>Send Mail</h2>
+                <p class="lead">Seu app de envio de e-mails particular!</p>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
 
+                    <?php if($mensagem->status['codigo_status'] == 200) { ?>
 
+                        <div class="container">
+                            <h1 class="display-4 text-success">Sucesso</h1>
+                            <p><?= $mensagem->status['descricao_status'] ?></p>
+                            <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+                        </div>
+
+                    <?php } ?>
+
+                    <?php if($mensagem->status['codigo_status'] == 500) { ?>
+
+                        <div class="container">
+                            <h1 class="display-4 text-danger">Erro</h1>
+                            <p><?= $mensagem->status['descricao_status'] ?></p>
+                            <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+                        </div>
+                        
+                     <?php } ?>
+                    
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
